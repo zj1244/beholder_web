75a637bd81f84e2d70fd725ce642180c0e26e119
zengjia <zengjia@chehejia.com>
2020-03-17 22:23:28 +0800

    fix

--- app/lib/mongo.py
+++ app/lib/mongo.py
@@ -5,12 +5,14 @@ from pymongo import MongoClient
 class MongoDB(object):
     def __init__(self, host='localhost', port=27017, database='', username='', password=''):
 
-
-
-
         self.host = host
         self.port = port
         self.database = database
-        self._conn = MongoClient('mongodb://%s:%s@%s:%s/' % (username, password, self.host, self.port), connect=False)
-        self.coll = self._conn[self.database]
+        # self._conn = MongoClient('mongodb://%s:%s@%s:%s/' % (username, password, self.host, self.port), connect=False)
+        if username and password:
+            self._conn = MongoClient(
+                'mongodb://%s:%s@%s:%s/' % (username, password, self.host, self.port), connect=False)
+        else:
+            self._conn = MongoClient(host=self.host, port=self.port, connect=False)
 
+        self.coll = self._conn[self.database]
